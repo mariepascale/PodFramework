2f5f1c2cf3603b6634d670747c87ec745ea7b0ff
mariepascale <marie.pascale.chauvel@gmail.com>
2019-05-19 03:16:36 -0400

    Finished pod framework

---
+++ PodFramework/PodFramework/PodFramework.h
@@ -0,0 +1,19 @@
+//
+//  PodFramework.h
+//  PodFramework
+//
+//  Created by Marie-Pascale Chauvel on 5/19/19.
+//  Copyright © 2019 Kaplan Test Prep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodFramework.
+FOUNDATION_EXPORT double PodFrameworkVersionNumber;
+
+//! Project version string for PodFramework.
+FOUNDATION_EXPORT const unsigned char PodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodFramework/PublicHeader.h>
+
+
---
+++ PodFramework/PodFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  PodFramework
+//
+//  Created by Marie-Pascale Chauvel on 5/19/19.
+//  Copyright © 2019 Kaplan Test Prep. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Did Some Stuff"
+    }
+}
